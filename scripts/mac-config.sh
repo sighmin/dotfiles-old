443bb1e6ea5513a445ac93228cbe170842029297
Simon van Dyk <simon.vandyk@gmail.com>
2018-11-22 22:27:52 +0000

    Add: key repeat and delay script and comment

--- scripts/mac-config.sh
+++ scripts/mac-config.sh
@@ -1,4 +1,5 @@
-defaults write -g KeyRepeat -int 1
-defaults write -g InitialKeyRepeat -int 10
+defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 defaults write com.apple.finder CreateDesktop false
 killall Finder
+echo "You need to restart for the changes to take effect"
